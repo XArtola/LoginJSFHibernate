8d50bf0845f85547d4583846aeb960289948c010
XArtola <43467002+XArtola@users.noreply.github.com>
2022-11-14 11:55:27 +0100

    3 laboaren zati bat

--- src/main/java/eredua/bean/LoginBean.java
+++ src/main/java/eredua/bean/LoginBean.java
@@ -1,9 +1,12 @@
 package eredua.bean;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import org.primefaces.event.SelectEvent;
 
@@ -12,7 +15,12 @@ public class LoginBean {
 	private String pasahitza;
 	private Date data;
 
+	private ErabiltzailearenMota mota;
+	private static List<ErabiltzailearenMota> motak = new ArrayList<ErabiltzailearenMota>();
+
 	public LoginBean() {
+		motak.add(new ErabiltzailearenMota(1, "ikaslea"));
+		motak.add(new ErabiltzailearenMota(2, "irakaslea"));
 	}
 
 	public String getIzena() {
@@ -53,6 +61,43 @@ public class LoginBean {
 
 	public void onDateSelect(SelectEvent event) {
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Data aukeratua: " + event.getObject()));
+	}
+
+	public ErabiltzailearenMota getMota() {
+		return mota;
+	}
+
+	public void setMota(ErabiltzailearenMota mota) {
+		this.mota = mota;
+		//System.out.println("Erabiltzailearen mota: " + mota.getKodea() + "/" + mota.getErabMota());
+	}
+
+	public List<ErabiltzailearenMota> getMotak() {
+		return motak;
+	}
+
+	public void setMotak(List<ErabiltzailearenMota> motak) {
+		this.motak = motak;
+	}
+
+	public static ErabiltzailearenMota getObject(String mota) {
+		for (ErabiltzailearenMota m : motak) {
+			if (mota.equals(m.getErabMota()))
+				return m;
+		}
+		return null;
+	}
+
+	public void listener(AjaxBehaviorEvent event) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage("Erabiltzailearen mota:" + mota.getKodea() + "/" + mota.getErabMota()));
+	}
+
+	public void onEventSelect(SelectEvent event) {
+		this.mota = (ErabiltzailearenMota) event.getObject();
+		// Egia esan, selection="#{login.mota}" atributuarekin ere lortzen da
+		FacesContext.getCurrentInstance().addMessage("nireForm:mezuak",
+				new FacesMessage("Erabiltzailearen mota (taula):" + mota.getKodea() + "/" + mota.getErabMota()));
 	}
 
 }
